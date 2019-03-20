55c5e51387e82a9ce1a3611cd664f7fbb3d65923
mrcallaghan <mcallaghan@naperville203.org>
2019-03-20 15:16:56 -0500

    finished reverseString method

--- ClassNotes/StringReverse.java
+++ ClassNotes/StringReverse.java
@@ -7,5 +7,25 @@
  */
 public class StringReverse
 {
-    
+    public static String reverse(String str)
+    {
+        // must have a terminating (base) case
+        if(str.equals(""))
+        {
+            return str;
+        }
+        
+        // must make the problem simplier
+        String firstChar = str.substring(0, 1);
+        String restOfString = str.substring(1);
+        
+        // must recurse -
+        //      call this method recursively with a simplier problem
+        String restOfStringReversed = reverse(restOfString);
+        
+        // return the solution
+        String strReversed = restOfStringReversed + firstChar;
+        return strReversed;
+        
+    }
 }
